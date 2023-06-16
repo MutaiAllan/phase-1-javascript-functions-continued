e8797a64d0bfcec6ff20e771b134ed77932c9e30
MutaiAllan <MutaiAllan>
2023-06-16 23:05:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(param = 'roller-skate') {
+    return (`This Saturday, I want to ${param}!`);
+}
+// console.log(saturdayFun());
+// console.log(saturdayFun('code'));
+
+function mondayWork(param = 'go to the office'){
+    return (`This Monday, I will ${param}.`);
+}
+// console.log(mondayWork());
+// console.log(mondayWork('start farming'));
+
+function wrapAdjective(param1 = '*') {
+    return function(param2 = 'special') {
+        return (`You are ${param1}${param2}${param1}!`);
+    }
+}
+const encouragingPromptFunction = wrapAdjective("!!!");
+// console.log(wrapAdjective("%")("a dedicated programmer"));
